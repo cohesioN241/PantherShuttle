84d35c76f4cc35be2f43283651eaba827f8c985a
Austin Duve <cohesioN241@gmail.com>
2011-12-06 22:52:59 -0600

    Update BusStopMap to use <Double, BusStop>

--- PantherShuttle/src/org/cs4880/panthershuttle/BusStopMap.java
+++ PantherShuttle/src/org/cs4880/panthershuttle/BusStopMap.java
@@ -4,29 +4,25 @@ import java.util.SortedMap;
 import java.util.TreeMap;
 
 public class BusStopMap {
-	private SortedMap<Double,String> busStopList;
+	private SortedMap<Double,BusStop> busStopList;
 	
 	public BusStopMap() {
-		busStopList = new TreeMap<Double,String>();
+		busStopList = new TreeMap<Double,BusStop>();
 	}
 	
-	public void addStop(String stopName, Double distance) {
-		busStopList.put(distance, stopName);
+	public void addStop(BusStop busstop, Double distance) {
+		busStopList.put(distance, busstop);
 	}
 	
 	public void removeStop(Double distance) {
 		busStopList.remove(distance);
 	}
 	
-	public String getClosestStop(){
-		return busStopList.get(busStopList.firstKey());
-	}
-	
 	public void clearStops(){
 		busStopList.clear();
 	}
 	
-	public SortedMap<Double,String> getMap(){
+	public SortedMap<Double,BusStop> getMap(){
 		return busStopList;
 	}
 	
